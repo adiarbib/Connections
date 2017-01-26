d1f92fa151e4d64a9c0eb507ae8319ea5f561353
adiarbib <adiarbib@gmail.com>
2017-01-26 11:00:33 +0200

    progress with Sign Up stuff

--- Connections/app/src/main/java/com/example/user/connections/MyUser.java
+++ Connections/app/src/main/java/com/example/user/connections/MyUser.java
@@ -3,42 +3,25 @@ package com.example.user.connections;
 import com.parse.ParseClassName;
 import com.parse.ParseUser;
 
+import java.util.Date;
+
 /**
  * Created by User on 25/01/2017.
  */
 @ParseClassName("MyUser")
 public class MyUser extends ParseUser {
     // constant keys for saving in parse
-    public static final String YEAR_KEY = "year";
-    public static final String MONTH_KEY = "month";
-    public static final String DAY_KEY = "day";
+    public static final String DATE_KEY = "date";
 
     public MyUser() {
     }
 
-    public int getYear() {
-        return getInt(YEAR_KEY);
+    public Date getBirthDate() {
+        return getDate(DATE_KEY);
     }
 
-    public void setYear(int year) {
-        put(YEAR_KEY, year);
+    public void setBirthate(Date date) {
+        put(DATE_KEY, date);
     }
-
-    public int getMonth() {
-        return getInt(MONTH_KEY);
-    }
-
-    public void setMonth(int month) {
-        put(MONTH_KEY, month);
-    }
-
-    public int getDay() {
-        return getInt(DAY_KEY);
-    }
-
-    public void setDay(int day) {
-        put(DAY_KEY, day);
-    }
-
 
 }
--- Connections/app/src/main/java/com/example/user/connections/SignUpActivity.java
+++ Connections/app/src/main/java/com/example/user/connections/SignUpActivity.java
@@ -1,13 +1,100 @@
 package com.example.user.connections;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.io.FileNotFoundException;
+import java.util.Date;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Calendar cal;
+    ImageButton profilePic;
+    EditText mailEditText;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button birthdateButt;
+    Button countryButt;
+    String formattedDate;
+    SimpleDateFormat df;
+    Date pickedDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        profilePic = (ImageButton) findViewById(R.id.img);
+        mailEditText = (EditText) findViewById(R.id.mail);
+        usernameEditText = (EditText) findViewById(R.id.signup_username_input);
+        passwordEditText = (EditText) findViewById(R.id.signup_password_input);
+        birthdateButt = (Button) findViewById(R.id.birthdate);
+        countryButt = (Button) findViewById(R.id.country);
+
+        cal = Calendar.getInstance();
+        df = new SimpleDateFormat("dd - MMM - yyyy");
+        formattedDate = df.format(cal.getTime());
+        pickedDate = cal.getTime();
+        birthdateButt.setText(formattedDate);
+
+        birthdateButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerDialog mDatePicker;
+                mDatePicker = new DatePickerDialog(SignUpActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedyear, int selectedmonth, int selectedday) {
+                        birthdateButt.setText(selectedday + " / " + selectedmonth + " / " + selectedyear);
+                        selectedmonth += 1;
+                        pickedDate = new Date(selectedyear, selectedmonth, selectedday);
+                    }
+                }, cal.get(java.util.Calendar.YEAR), cal.get(java.util.Calendar.MONTH), cal.get(java.util.Calendar.DAY_OF_MONTH));
+
+                mDatePicker.setTitle("Select Date");
+                mDatePicker.show();
+
+            }
+        });
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri targetUri = data.getData();
+            //textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                profilePic.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException();
+            }
+        }
     }
 }
